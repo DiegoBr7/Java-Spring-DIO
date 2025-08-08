6546122b300f4a415fd2fa6258ae6ad96129c846
Diego <diegobrasileiro8@gmail.com>
2025-08-08 11:03:32 -0300

    [08082025] feat: iniciando o projeto

---
+++ demo/src/main/java/br/com/dio/persistence/dao/BoardColumnDAO.java
@@ -0,0 +1,105 @@
+package br.com.dio.persistence.dao;
+
+import br.com.dio.dto.BoardColumnDTO;
+import br.com.dio.persistence.entity.BoardColumnEntity;
+import com.mysql.cj.jdbc.StatementImpl;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import static br.com.dio.persistence.entity.BoardColumnKindEnum.findByName;
+
+@RequiredArgsConstructor
+public class BoardColumnDAO {
+
+        private final Connection connection ;
+
+        public BoardColumnEntity insert(final BoardColumnEntity entity) throws SQLException {
+            var sql = "INSERT INTO BOARDS_COLUMNS (name , `order` , kind , board_id) VALUES ( ?, ?, ?, ? );";
+
+            try ( var statement = connection.prepareStatement(sql)){
+                var i= 1;
+                statement.setString(i++ , entity.getName()  );
+                statement.setInt(i++ , entity.getOrder());
+                statement.setString(i++ , entity.getKind().name());
+                statement.setLong(i , entity.getBoard().getId());
+                statement.executeUpdate();
+                if (statement instanceof StatementImpl impl){
+                    entity.setId(impl.getLastInsertID());
+                }
+                return entity;
+            }
+
+        }
+
+    public List<BoardColumnEntity> findByBoardId(Long id) throws SQLException{
+
+            List<BoardColumnEntity> entities = new ArrayList<>();
+
+            var sql = "SELECT id , name , `order` FROM BOARDS_COLUMNS WHERE board_id = ? ORDER BY `order` ";
+
+            try(var statment = connection.prepareStatement(sql)){
+                statment.setLong(1,id);
+                statment.executeQuery();
+                var resultSet = statment.getResultSet();
+                while (resultSet.next()){
+                    var entity = new BoardColumnEntity();
+                    entity.setId(resultSet.getLong("id"));
+                    entity.setName(resultSet.getString("name"));
+                    entity.setName(resultSet.getString("order"));
+                    entity.setKind(findByName(resultSet.getString("kind")));
+                    entities.add(entity);
+
+                }
+           return entities;
+            }
+        }
+
+
+
+        public List<BoardColumnEntity> findByBoardIdWithDetails(Long id) throws SQLException{
+
+            List<BoardColumnDTO> dtos = new ArrayList<>();
+
+            var sql = """
+                    
+                    SELECT bc.id, 
+                    bc.name, bc.`order`,
+                    bc.kind,
+                    COUNT (SELECT c.id 
+                    FROM CARDS c 
+                    WHERE c.board_column_id = bc.id) cards_amount
+                    FROM BOARDS_COLUMNS bc
+                    WHERE board_id = ? 
+                    ORDER BY `order` 
+                    
+                    """;
+
+            try(var statement = connection.prepareStatement(sql)) {
+                statement.setLong( 1, id);
+                statement.executeQuery();
+                var resultSet = statement.getResultSet();
+                while (resultSet.next()) {
+                    var dto = new BoardColumnDTO(
+                            resultSet.getLong( "bc.id"),
+                            resultSet.getString( "bc.name"),
+                            resultSet.getInt( "bc.order"),
+                            findByName(resultSet.getString( "bc.kind")),
+                            resultSet.getInt(  "cards_amount")
+                    );
+                    dtos.add(dto);
+                }
+                return dtos;
+            }
+        }
+
+    public Object findByBoardIDWithDetails(Long id) {
+    }
+}
+
